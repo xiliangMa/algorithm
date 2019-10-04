5961a20432593736f58d9405095d8f4f7c4dcf8d
xiliangMa <xiliangMa@outlook.com>
2019-10-04 19:14:02 +0800

    update heap sort code by go
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- go/01-sort/heap_sort_test.go
+++ go/01-sort/heap_sort_test.go
@@ -3,15 +3,15 @@ package _1_sort
 import "testing"
 
 /**
- *  堆排序 heapsort
+ *
  *  parent = (i+1)/2
  *  左子节点： c1 = 2i + 1
  *  右子节点： c2 = 2i + 2
  *	n 为数组长度
  *  i 为任意指定的节点
  */
-func Heap_Sort(tree []int, n, i int) []int {
-	if (i >= n) {
+func Heapify(tree []int, n, i int) []int {
+	if i >= n {
 		return tree
 	}
 	c1 := 2*i + 1
@@ -28,16 +28,37 @@ func Heap_Sort(tree []int, n, i int) []int {
 
 	if max != i {
 		tree[max], tree[i] = tree[i], tree[max]
-		Heap_Sort(tree, n, max)
+		Heapify(tree, n, max)
 	}
 
+	return tree
+}
+
+func Build_Heap(tree []int, n int) []int {
+	last_node := n - 1
+	parent := (last_node - 1) / 2
+
+	for i := parent; i >= 0; i-- {
+		Heapify(tree, n, i)
+	}
+
+	return tree
+}
+
+func Heap_Sort(tree []int, n int) []int {
+	heap := Build_Heap(tree, n)
+	for i := n - 1; i >= 0; i-- {
+		// 交换根节点和最后一个节点
+		heap[i], heap[0] = heap[0], heap[i]
+		Heapify(heap, i, 0)
+	}
 	return tree
 }
 
 /**
  * 最终输出结果 为： 堆排序（递归）---后 [10 5 3 4 1 2]
  */
-func Test_Heap_Sort(t *testing.T){
-	tree := []int{4, 10, 3, 5, 1, 2}
-	t.Log("堆排序（递归）---后", Heap_Sort(tree, 6, 0))
+func Test_Heap_Sort(t *testing.T) {
+	tree := []int{2, 5, 3, 1, 10, 4}
+	t.Log("堆排序（递归）---后", Heap_Sort(tree, 6))
 }

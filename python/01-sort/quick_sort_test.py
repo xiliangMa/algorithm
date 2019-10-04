81555b750ac6a80b5ae18027cae6828c33b3ba30
xiliangMa <xiliangMa@outlook.com>
2019-10-04 11:10:29 +0800

    add quick sort no new arr
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- python/01-sort/quick_sort_test.py
+++ python/01-sort/quick_sort_test.py
@@ -17,6 +17,36 @@ def Test_Qsort(arrs):
     else:
         return arrs
 
+'''
+    快速排序(1) quick sort
+    避免额外的内存开销 去除前面left、right 数组
+'''
+def Test_Qsort2(arrs, start, end):
+    if start >= end:
+        return
+    pivod = arrs[end]
+    left = start
+    right = end - 1
+    while left <= right:
+        while left <= right and arrs[left] < pivod:
+            left += 1
+        while left <= right and arrs[right] > pivod:
+            right -= 1
+        if left >= right:
+            break
+        arrs[left], arrs[right] = arrs[right], arrs[left]
+    arrs[left], arrs[end] = arrs[end], arrs[left]
+    Test_Qsort2(arrs, start, left - 1)
+    Test_Qsort2(arrs, left + 1, end)
+
+    return arrs
+
+
+
 if __name__ == '__main__':
     arrs = [3, 1, 6, 4, 5, 0, 8, 9, 7, 2]
-    print "快速排序---后：", Test_Qsort(arrs)
+    print "快速排序(1)---后：", Test_Qsort(arrs)
+
+    arrs = [3, 1, 6, 4, 5, 0, 8, 9, 7, 2]
+    print "快速排序(2)---后：", Test_Qsort2(arrs, 0, 9)
+

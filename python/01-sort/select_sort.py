a514ec66f3620f35c099846c91b9e1e70fba6559
xiliangMa <xiliangMa@outlook.com>
2019-10-04 15:05:12 +0800

    add select sort code by python
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

---
+++ python/01-sort/select_sort.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+
+'''
+    选择排序（1） bubble sort 递归
+'''
+def Ssort(arrs):
+    for i in range(len(arrs) - 1):
+        min = arrs[i]
+        if arrs[i+1] < min:
+            min = arrs[i+1]
+            arrs[i], arrs[i+1] = arrs[i+1], arrs[i]
+            Ssort(arrs)
+    return arrs
+
+if __name__ == '__main__':
+    arrs = [3, 1, 6, 4, 5, 0, 8, 9, 7, 2]
+    print("选择排序(递归)---后：",Ssort(arrs))
+
+
+
+

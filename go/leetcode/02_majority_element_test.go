5a397b9653af4bc8504593e80fe7bac6101b8694
xiliangMa <xiliangMa@outlook.com>
2019-10-05 16:18:57 +0800

    add  majority element code by go
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- go/leetcode/02_majority_element_test.go
+++ go/leetcode/02_majority_element_test.go
@@ -1,1 +1,32 @@
 package leetcode
+
+import "testing"
+
+/**
+ * 求众数
+ */
+
+func Majority_Element(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	num_map := make(map[int]int)
+	for _, value := range nums {
+		if _, ok := num_map[value]; ok {
+			num_map[value] += 1
+			if num_map[value] > len(nums) / 2 {
+				return value
+			}
+		} else {
+			num_map[value] = 1
+		}
+	}
+
+	return 0
+}
+
+func Test_Majority_Element(t *testing.T){
+	nums := []int{3, 2, 3}
+	t.Log(Majority_Element(nums))
+}

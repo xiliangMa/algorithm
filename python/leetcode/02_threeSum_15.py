cd6b0a6b56eaf70c391c515dbedeccbad672ad12
xiliangMa <xiliangMa@outlook.com>
2019-10-06 17:08:19 +0800

    add three sum code by pyhton
    Signed-off-by: xiliangMa <xiliangMa@outlook.com>

--- python/leetcode/02_threeSum_15.py
+++ python/leetcode/02_threeSum_15.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -16,7 +18,6 @@ class Solution(object):
                 temp2 = nums[:]
                 temp2.remove(a)
                 temp2.remove(b)
-
                 if 0 - a - b in temp2:
                     if flag == False:
                         result = []
@@ -26,12 +27,45 @@ class Solution(object):
                         result.sort()
                         if result not in data:
                             data.append(result)
-
                     if a == b == 0:
                         flag = True
         return data
 
+    def threeSum1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        data = []
+        length = len(nums)
+        if length < 3:
+            return []
+        for i in range(length):
+            left = i + 1
+            right = length - 1
+            while left < right:
+                result = []
+                if nums[i] + nums[left] + nums[right] == 0:
+                    result.append(nums[i])
+                    result.append(nums[left])
+                    result.append(nums[right])
+                    result.sort()
+                    if result not in data:
+                        data.append(result)
+                    left += 1
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+        return data
+
+
+
+
+
 if __name__ == '__main__':
     s = Solution()
-    nums = [12,0,3,-14,5,-11,11,-5,-2,-1,6,-7,-10,1,4,1,1,9,-3,6,-15,0,6,1,6,-12,3,7,11,-6,-8,0,9,3,-7,-1,7,-10,1,13,-4,-7,-9,-7,9,3,1,-13,-3,13,8,-11,-9,-8,-3,4,-13,7,-11,5,-14,-4,-9,10,6,-9,-6,-9,-12,11,-11,-9,11,-5,0,-3,13,-14,-1,-13,7,-7,14,5,0,-4,-6,-6,-11,-2,14,-10,2,12,8,-7,-11,-13,-9,14,5,-5,-9,1,-2,6,5,-12,-1,-10,-9,-9,-10,12,11]
-    print "三数之和", s.threeSum(nums)
+    nums = [-1, 0, 1, 2, -1, -4]
+    print "三数之和", s.threeSum1(nums)
